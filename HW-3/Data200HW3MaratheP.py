000aece272eddbfabf853cb3791018172c428ef9
Parth-05 <parthmarathe5@gmail.com>
2023-09-19 14:44:36 -0700

    Code cleaning

--- HW-3/Data200HW3MaratheP.py
+++ HW-3/Data200HW3MaratheP.py
@@ -3,14 +3,20 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
+# Title
 st.title("Homework 3")
 st.write('Name: Parth Chinmaya Marathe')
 st.write('Student ID: 017411199')
+# getting path of directory
 path = os.path.dirname(__file__)
-myfile_path = path + '/toy_dataset.csv'
-data = pd.read_csv(myfile_path)
+# dataset path
+dataset_path = path + '/toy_dataset.csv'
+# reading the dataset
+data = pd.read_csv(dataset_path)
+
 gen_count = data['Gender'].value_counts()
 
+# Plot 1
 fig1, ax1 = plt.subplots()
 ax1.bar(gen_count.index, gen_count.values, color='orange')
 ax1.set_xlabel('Gender')
@@ -21,6 +27,7 @@ st.pyplot(fig1)
 
 df_grouped = data.groupby('City')['Income'].mean().reset_index()
 
+# Plot 2
 fig2, ax2 = plt.subplots()
 ax2.bar(df_grouped['City'], df_grouped['Income'], color='blue')
 ax2.set_xlabel('City')
