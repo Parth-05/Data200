61eb469af6d037949970bb3e8c4099f0b26246bc
Parth-05 <parthmarathe5@gmail.com>
2023-09-19 13:34:17 -0700

    Updated file path

--- HW-3/Data200HW3MaratheP.py
+++ HW-3/Data200HW3MaratheP.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 st.title("Homework 3")
 st.write('Name: Parth Chinmaya Marathe')
 st.write('Student ID: 017411199')
-file_path = os.path.join(os.getcwd(), "toy_dataset.csv")
-data = pd.read_csv(file_path)
+# file_path = os.path.join(os.getcwd(), "toy_dataset.csv")
+data = pd.read_csv("toy_dataset.csv")
 gen_count = data['Gender'].value_counts()
 
 fig1, ax1 = plt.subplots()
